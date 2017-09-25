748a72ac8a5a50c5983edda171939ea57c9aa335
okkhoury <okk6nb@virginia.edu>
2017-09-24 20:47:03 -0400

    fixing google import problems

--- api/views.py
+++ api/views.py
@@ -6,9 +6,9 @@ import os
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
-# from google.cloud import speech
-# from google.cloud.speech import enums
-# from google.cloud.speech import types
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
 
 # Create your views here.
 
@@ -24,37 +24,37 @@ def getResponse(request):
         return JsonResponse(response, safe=False)
 
 
-# def convertAudioFileToText(filename):
-# 	# Instantiates a client
-# 	client = speech.speechClient();
+def convertAudioFileToText(filename):
+	# Instantiates a client
+	client = speech.speechClient();
 
-# 	# The name of the audio file to transcribe
-# 	file_name = os.path.join(
-#     os.path.dirname(__file__), filename)
+	# The name of the audio file to transcribe
+	file_name = os.path.join(
+    os.path.dirname(__file__), filename)
 
-#     # Loads the audio into memory
-# 	with io.open(file_name, 'rb') as audio_file:
-# 	    content = audio_file.read()
-# 	    audio = types.RecognitionAudio(content=content)
-
-
-# 	config = types.RecognitionConfig(
-# 	    encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-# 	    sample_rate_hertz=44100,
-# 	    language_code='en-US')
+    # Loads the audio into memory
+	with io.open(file_name, 'rb') as audio_file:
+	    content = audio_file.read()
+	    audio = types.RecognitionAudio(content=content)
 
 
-# 	# Detects speech in the audio file
-# 	response = client.recognize(config, audio)
-# 	alternatives = response.results[0].alternatives
+	config = types.RecognitionConfig(
+	    encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+	    sample_rate_hertz=44100,
+	    language_code='en-US')
 
-# 	textFromFile = ""
 
-# 	for alternative in alternatives:
-# 		textFromFile += 'Transcript: {}'.format(alternative.transcript)
-# 	    #print('Transcript: {}'.format(alternative.transcript))
+	# Detects speech in the audio file
+	response = client.recognize(config, audio)
+	alternatives = response.results[0].alternatives
 
-# 	return textFromFile;
+	textFromFile = ""
+
+	for alternative in alternatives:
+		textFromFile += 'Transcript: {}'.format(alternative.transcript)
+	    #print('Transcript: {}'.format(alternative.transcript))
+
+	return textFromFile;
 
 
 
