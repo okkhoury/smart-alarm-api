e581f28f8227a4457747637643c8e75454beb6fe
okkhoury <okk6nb@virginia.edu>
2017-10-24 14:11:44 -0400

    changed speech api to use IBM watson. works now

--- api/views.py
+++ api/views.py
@@ -1,25 +1,32 @@
-# -*- coding: utf-8 -*-
+ # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 import io
 import os
 
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
+# from google.cloud import speech
+# from google.cloud.speech import enums
+# from google.cloud.speech import types
 
-from googleapiclient.discovery import build
-from oauth2client.client import GoogleCredentials
-
+# from googleapiclient.discovery import build
+# from oauth2client.client import GoogleCredentials
 
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
-
-
 from django.core.files.storage import FileSystemStorage
 
 from .forms import DocumentForm
+
+import json
+from watson_developer_cloud import SpeechToTextV1
+
+import re
+
+myUsername = '6d1d6655-b091-4232-bb3c-72f35b924f63'
+myPassword = 'PiNCxqb5uTbe'
+
+speech_to_text = SpeechToTextV1(username=myUsername, password = myPassword, x_watson_learning_opt_out=False)
 
 # Create your views here.
 def index(request):
@@ -55,7 +62,21 @@ def getResponse(request):
 
 			handle_uploaded_file(file)
 
-			response['textFromFile'] = convertAudioFileToText("saved.flac");
+			#response['textFromFile'] = convertAudioFileToText("saved.flac");
+
+			with io.open("saved.flac", 'rb') as audio_file:
+				fileData = json.dumps(speech_to_text.recognize(audio_file, content_type='audio/flac', timestamps=True, word_confidence=True))
+
+			
+			data = re.sub(r'([^\s\w]|_)+', '', fileData.split("transcript")[1].split("timestamps")[0]).strip()
+
+			for word in data.split(" "):
+				if word == "Tony":
+					response['transcript'] = "tony was here"
+					return JsonResponse(response, safe=False)
+
+
+			response['transcript'] = data
 		
 			return JsonResponse(response, safe=False)
 
