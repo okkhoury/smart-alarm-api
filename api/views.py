82aa3c73dd808a8f315cb310e9ab776fc4e59fb2
okkhoury <okk6nb@virginia.edu>
2017-12-08 12:24:49 -0500

    fixed prob with twenty

--- api/views.py
+++ api/views.py
@@ -59,7 +59,7 @@ snoozeWords = ["snooze", "news"]
 units = [
         "oh", "one", "two", "three", "four", "five", "six", "seven", "eight",
         "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
-        "sixteen", "seventeen", "eighteen", "nineteen", "twenty"
+        "sixteen", "seventeen", "eighteen", "nineteen"
       ]
 
 tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty"]
